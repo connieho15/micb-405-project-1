cc2d088e027d4530e0ba6866bd791185d14d0af9
Connie Ho <connieho15@users.noreply.github.com>
2018-10-19 13:21:20 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 
 bwa_index ref_genome.fasta
 
-for filename in Bat Cat Guinea_pig Patient_10 Patient_11 Patient_12 Patient_13 Patient_14 Patient_15 Patient_16 Patient_1 Patient_2 Patient_3 Patient_4 Patient_5 Patient_6 Patient_7 Patient_8 Patient_9 Rabid_racoon_1 Rabid_racoon_2 ; do
-	echo "Processing alignment for - $filename"
-    bwa mem ref_genome.fasta "~/project_1/rawdata/project_1/${filename}_1.fastq.gz ~/project_1/rawdata/project_1/${filename}_2.fastq.gz > ${filename}_outfile.sam"
+for filename in Bat Cat Guinea_pig Patient_10 Patient_11 Patient_12 Patient_13 Patient_14 Patient_15 Patient_16 Patient_1 Patient_2 Patient_3 Patient_4 Patient_5 Patient_6 Patient_7 Patient_8 Patient_9 Rabid_raccoon_1 Rabid_raccoon_2 Rabid_raccoon_3 Rabid_raccoon_4 ; do
+    echo "Processing alignment for - $filename"
+    bwa mem ref_genome.fasta ${filename}_1.fastq.gz ${filename}_2.fastq.gz > ${filename}_outfile.sam
+    echo "executed command bwa mem ref_genome.fasta ${filename}_1.fastq.gz {filename}_2.fastq.gz > ${filename}_outfile.sam"
 done
